d40138fcb8ac4f73ed024762beaf2a74b916a956
ed-knowah <edetnoah@gmail.com>
2021-02-26 01:24:01 +0100

    worked on the update(PUT) route and added method-override

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const URL = "mongodb://localhost/tester_blog_app";
 
+//  APP CONFIG
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 (async function () {
   try {
@@ -24,7 +31,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // const connectWithRetry = () => {
 //     console.log('MongoDB connection with retry')
-//     mongoose.connect('mongodb://localhost/users_test').then(()=>{
+//     mongoose.connect(URL).then(()=>{
 //       console.log('MongoDB is connected')
 //     }).catch(err=>{
 //       console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
@@ -72,44 +79,46 @@ app.post("/blogs", (req, res) => {
     if (err) {
       console.log("error", err);
     } else {
-      res.redirect("/blogs"); 
+      res.redirect("/blogs");
     }
   });
 });
 
 // SHOW ROUTE
-app.get("/blogs/:id", (req,res)=>{
-
-
-
-  Blog.findById(req.params.id, (err, foundBlog)=>{
-    if(err){
+app.get("/blogs/:id", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
       console.log("error", err);
       res.redirect("/blogs");
-    }else {
-      res.render("show", {blog: foundBlog});
+    } else {
+      res.render("show", { blog: foundBlog });
     }
-
   });
 });
 
 // EDIT ROUTES
-app.get("/blogs/:id/edit", (req,res)=>{
-  
-
-  Blog.findById(req.params.id, (err, foundBlog)=>{
-   
-    if(err){
-      res.redirect("/blogs")
+app.get("/blogs/:id/edit", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err) {
+      res.redirect("/blogs");
       console.log("error", err);
-    }else{
-      res.render("edit", {blog: foundBlog});
+    } else {
+      res.render("edit", { blog: foundBlog });
     }
   });
 });
 
-
-
+// UPDATE ROUTE
+app.put("/blogs/:id", (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+    if (err) {
+      console.log("error".err);
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Your blog is running on localhost:${PORT}`);
