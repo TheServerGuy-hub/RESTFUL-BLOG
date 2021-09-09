56348033df320a015d318bb48ea29ef75387b820
Edet Noah <edetnoah@gmail.com>
2021-09-09 11:12:55 +0100

    update userRoute

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/login", userController.getForm);
+router.get("/register", userController.getRegisterForm);
 
 router.post("/register", userController.createUser);
 router.post("/login", userController.loginUser);
